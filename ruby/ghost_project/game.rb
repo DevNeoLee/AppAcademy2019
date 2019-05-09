0df6bedd9523a440526a3c42812733321c3dab30
devneolee <devneolee@gmail.com>
2019-05-09 15:38:07 -0700

    Create Ghost_Project

---
+++ ruby/ghost_project/game.rb
@@ -0,0 +1,36 @@
+require_relative "player.rb"
+
+class Game
+
+    def initialize
+        @fragment = ""
+        @players
+        @dictionary
+    end
+
+    def valid_play?(str)
+
+    end
+
+    def play_round
+
+    end
+
+    def current_player
+
+    end
+
+    def previous_player
+
+    end
+
+    def next_player!
+
+    end
+
+    def take_turn(player)
+
+    end
+
+    
+end
