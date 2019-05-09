9ef83d28a4bceed99461f0420cf9620db175c0ad
devneolee <devneolee@gmail.com>
2019-05-08 20:51:22 -0700

    Update enumerables

--- ruby/my_enumerables.rb
+++ ruby/my_enumerables.rb
@@ -18,5 +18,41 @@ class Array
     #     self
     # end
 
-    
+    def my_select(&prc)
+        selects = []
+
+        self.my_each {|item| selected << item if prc.call(item)}
+
+        selects
+    end
+
+    def my_select(&prc)
+        selects = []
+
+        self.my_each do |item|
+        selects << item if prc.call(item)
+        end
+
+        selects
+    end
+
+    def my_flatten
+      flattened = []
+
+      self.each do |ele|
+        if ele.instance_of?(Array)
+            flattened.concat(ele.my_flatten)
+        else
+            flattened << ele
+        end
+      end 
+
+      flattened
+    end
+
+   def factors(num)
+      num % 
+   end
+
+
 end
