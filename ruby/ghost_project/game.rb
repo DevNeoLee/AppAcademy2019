8b932aeb4bd6d7e29bbf3412f3281460f3e49207
devneolee <devneolee@gmail.com>
2019-05-09 17:54:22 -0700

    Update enumerable ghost_project

--- ruby/ghost_project/game.rb
+++ ruby/ghost_project/game.rb
@@ -1,23 +1,39 @@
-require_relative "player.rb"
+require_relative "player"
+require 'set'
+
 
 class Game
+    attr_reader :fragment, :players
 
-    def initialize
+    def initialize(player1, player2)
         @fragment = ""
-        @players
-        @dictionary
+
+        @players = [player1, player2] 
+    end
+
+    def dictionary(data = nil)
+        @dictionary = data ||=  File.readlines("dictionary.txt").join("\n").split
+        @dictionary.to_set
+        
     end
 
     def valid_play?(str)
+        alphabet = ("a".."z").to_a
 
+        return false unless alphabet.include?(str)
+        true
     end
 
     def play_round
-
+        puts "+++++++++++++++++++++++++++++"
+        puts "Welcome to the game of Ghost"
+        puts "+++++++++++++++++++++++++++++"
+        puts
+     
     end
 
     def current_player
-
+        @players
     end
 
     def previous_player
@@ -31,6 +47,15 @@ class Game
     def take_turn(player)
 
     end
-
     
 end
+
+obj = Game.new("adam", "peter")
+
+p obj.dictionary
+
+p obj.dictionary.class
+
+
+      
+
