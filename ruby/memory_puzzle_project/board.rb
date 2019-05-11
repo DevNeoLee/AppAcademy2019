8f5aa3505cbaf3122c1ae9fdd534233a46939eb1
devneolee <devneolee@gmail.com>
2019-05-10 19:58:27 -0700

    Create Memory_puzzle

--- ruby/memory_puzzle_project/board.rb
+++ ruby/memory_puzzle_project/board.rb
@@ -1,23 +1,53 @@
+require_relative "card"
 
 class Board
+    attr_reader :size
 
+    def initialize(n = 4)
+        @grid = Array.new(n) {Array.new(n)}
+        @size = size
+        populate
+    end
+
+    def [](pos)
+        row, col = pos
+        @grid[row][col]
+    end
+
+    def []=(pos, value)
+        row, col = pos
+        @grid[row][col] = value
+    end
 
     def populate
-
+        num_pairs = size**2 / 2
+        cards = Card.shuffled_pairs(num_pairs)
+        grid.each_index do |i|
+            grid[i].each_index do |j|
+                self[[i, j]] = cards.pop 
+            end
+        end        
     end
 
     def render
-
+        system('clear')
+        puts " #{(0...size).to_a.join(" ")}"
+        grid.each_with_index do |row, i|
+            puts "#{i} : #{row.join(" ")}"
+        end
     end
 
     def won?
-
+        grid.all? do |row|
+            row.all?(&:revealed?)
+        end
     end
 
-    def reveal
-
+    def revealed?(pos)
+        self[pos].revealed?
     end
 
+    private
 
-
+    attr_reader :grid
 end
--- ruby/memory_puzzle_project/card.rb
+++ ruby/memory_puzzle_project/card.rb
@@ -1,24 +1,43 @@
 
 class Card
-    
-    def initialize(value, face )
+    VALUES = ("A".."Z").to_a
+
+    def self.shuffled_pairs(num_pairs)
+        values = VALUES
+
+        while num_pairs > values.length
+            values = values + values
+        end
+
+        values = values.shuffle.take(num_pairs) * 2
+        values.shuffle!
+        values.map {|val| self.new(val)}
+    end
+
+    attr_reader :value
+     
+    def initialize(value, face = false)
         @value = value
-        @face = true
+        @face = face
     end
 
     def hide
-
+        @face = false
     end
 
     def reveal
-
+        @face = true
     end
 
     def to_s
-
+        if @face == true
+            return @value
+        else
+            " "
+        end
     end
 
-    def ==()
-
+    def ==(other)
+        self.value == other.value
     end
 end
