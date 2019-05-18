a591c817870df1c7f950e69ed6b7e4f2b69e7671
devneolee <devneolee@gmail.com>
2019-05-18 15:42:11 -0700

    Update plog

--- rails/plog/app/controllers/plog_controller.rb
+++ rails/plog/app/controllers/plog_controller.rb
@@ -3,5 +3,27 @@ class PlogController < ApplicationController
         @posts = Post.all.reverse
     end
 
+    def create
+        Post.create(user: params[:input1], message: params[:input2])
+        redirect_to :root
+    end
+
+    def delete
+        Post.find(params[:id]).destroy
+        redirect_to :root
+    end
+
+    def edit
+        @post= Post.find(params[:id])
+    end
+
+    def update
+        pp = Post.find(params[:id])
+        pp.user = params[:input1]
+        pp.message = params[:input2]
+        pp.save
+        redirect_to :root
+    end
+
 
 end
--- rails/plog/config/routes.rb
+++ rails/plog/config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   root 'plog#index'
+  get 'plog/create'
+  get 'plog/delete'
+  get 'plog/edit'
+  get 'plog/update'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
