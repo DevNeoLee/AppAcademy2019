cf471224060593e85cf0c1082629713ed5c4e4db
devneolee <devneolee@gmail.com>
2019-05-17 19:36:28 -0700

    Update blog_1

--- rails/blog_1/blog_1/app/controllers/blog_controller.rb
+++ rails/blog_1/blog_1/app/controllers/blog_controller.rb
@@ -1,10 +1,23 @@
 class BlogController < ApplicationController
     def index
-        @posts = Post.all
+        @posts = Post.all.reverse
     end
 
     def create
         Post.create(writer: params[:irum], content: params[:naeyong])
+        redirect_to :root
+    end
+
+    def destroy
+        Post.find(params[:id]).destroy
+        redirect_to :root
+    end
+
+    def update
+        pp = Post.find(params[:id])
+        pp.writer = params[:irum]
+        pp.content = params[:naeyong]
+        pp.save
         redirect_to :root
     end
 end
--- rails/blog_1/blog_1/config/routes.rb
+++ rails/blog_1/blog_1/config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root 'blog#index'
   get '/blog/create' => 'blog#create'
+  get '/blog/destroy' => 'blog#destroy'
+  get '/blog/update'
   #== get '/blog/create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
