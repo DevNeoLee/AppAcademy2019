faad2aadbd548074f32cc11991c5176a42570e82
devneolee <devneolee@gmail.com>
2019-05-20 11:08:12 -0700

    Update timeline

--- rails/timeline/app/controllers/timeline_controller.rb
+++ rails/timeline/app/controllers/timeline_controller.rb
@@ -1,7 +1,15 @@
 class TimelineController < ApplicationController
     def index
+        @blogs = Blog.all.reverse
     end
 
     def write
+        Blog.create(content: params[:naeyong])
+        redirect_to :root
+    end
+
+    def comment
+        Comment.create(blog_id: params[:blog_id], msg: params[:comment])
+        redirect_to :root
     end
 end
