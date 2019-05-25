e658d8150dfb0a5a76bc3f249a1e41ba20447bf4
devneolee <devneolee@gmail.com>
2019-05-24 18:50:21 -0700

    Update website

--- rails/website_1/app/controllers/author_controller.rb
+++ rails/website_1/app/controllers/author_controller.rb
@@ -1,4 +1,10 @@
 class AuthorController < ApplicationController
     def index
+        @posts = Post.all
+    end
+
+    def create
+        Post.create(person: params[:name], description: params[:description])
+        redirect_to :root
     end
 end
