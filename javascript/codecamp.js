131c703dce5e3d91f470e77b0a591d66f4a789d6
devneolee <devneolee@gmail.com>
2019-05-26 18:16:49 -0700

    Update javascript

--- javascript/codecamp.js
+++ javascript/codecamp.js
@@ -1,3 +1,61 @@
+// html 다큐먼트 에서 JS파일을 <script scr=""></script> 안에서 불러주고 콘솔에서 오픈해야함.
+
 // comment
 /* this is a
 multi-line comment */
+
+/* Data types 
+undefined, null boolean, string, symbol, number, object */
+
+//variable
+// var
+// var myName ="beau"
+// let
+// let ourName ="brian"
+// const
+// contst pi = 3.14
+
+// declarations
+var a = 5;
+var b = 2;
+a = b;
+var c = "I am who I am";
+
+// go go!
+
+var quo = 11 % 3 ;
+var ourDecimal = 5.7;
+var myDecimal = 0.009;
+var product = ourDecimal * myDecimal;
+var prod1 = quo / ourDecimal;
+
+//a += 1
+//a -= 1
+// a *= 1
+// a /= 1
+
+// b -- 
+// b ++
+
+
+//var str = "alright"; 
+//var str1 = 'this also alright';
+
+var mystr = "I said \"James\'s partner\" " ;
+
+//back tick ` `
+
+// \'
+// \n newline
+// \t tab
+// \b backspace
+
+var myStr = "FirstLine\n\tSecondLine\nThirdLine";
+
+// concatenating
+var ab = mystr + " dance ";
+ab += ab;
+
+
+console.log(quo);
+console.log(ab);
