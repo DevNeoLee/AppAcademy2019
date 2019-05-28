691792edec084558cca8170a3774f648a61ecbc1
devneolee <devneolee@gmail.com>
2019-05-27 20:41:27 -0700

    Update javascript

--- javascript/homework1.js
+++ javascript/homework1.js
@@ -1,13 +1,27 @@
 // fizzBuzz, takes an arr, returns a new arr every # is divisible by either 3 or 5 but not both
 
 let fizzBuzz = function(arr){
+    const fizzbuzzArr = [];
 
-    let newArr = arr.map { |ele|
-        ((ele % 3 == 0 && ele % 5 != 0) || (ele % 5 == 0 && ele % 3 != 0))
-    }
-    return newArr
+    arr.foreach(el => {
+       if((ele % 3 == 0 && ele % 5 != 0) || (ele % 5 == 0 && ele % 3 != 0)) {
+           fizzbuzzArr.push(el);
+       }
+    });
+    return fizzBuzzArr;
 }
 
+function fizzBuzz(array) {
+    const fizzBuzzArr = [];
+
+    array.forEach(el => {
+        if ((el % 3 === 0) ^ (el % 5 === 0)) {
+            fizzBuzzArr.push(el);
+        }
+    });
+
+    return fizzBuzzArr;
+}
 let isPrime = functioin(number){
     if (number < 2){
         return false;
