268d85292d178786f748408512382d1545e7079d
devneolee <devneolee@gmail.com>
2019-05-29 00:20:54 -0700

    Create reactApp

---
+++ react/react_crash_todo/src/App.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import Todos from './components/Todos';
+
+import './App.css';
+
+class App extends Component {
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: 'Take out the trash',
+        completed: false
+      },
+      {
+        id: 2,
+        title: 'Dinner with wife',
+        completed: true
+      },
+      {
+        id: 3,
+        title: 'Meeting with boss',
+        completed: false
+      },
+    ]
+  }
+
+  markComplete =(id) => {
+    this.setState( {todos: this.state.todos.map(todo => {
+      if(todo.id === id) {
+        todo.completed = !todo.completed
+      }
+      return todo;
+    })});
+  }
+  render() {
+ 
+  return (
+    <div className="App">
+      <Todos todos={this.state.todos} markComplete={this.markComplete}/>
+    </div>
+  );
+}
+}
+export default App;
