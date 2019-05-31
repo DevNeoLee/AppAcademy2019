8a1f8df22a26388f2db029a1c0e62547fbf5f534
devneolee <devneolee@gmail.com>
2019-05-30 21:59:31 -0700

    Update react todo_list

--- react/todo_list/src/components/Todos.js
+++ react/todo_list/src/components/Todos.js
@@ -2,18 +2,18 @@ import React, { Component } from 'react';
 
 class Todos extends Component {
     render() {
-        return (
-            <h1>올라 올라 {this.props.da[1].id} </h1>
-        );
-        return this.props.abc.map((hoho) => (
-            <h1>올라...1
+       
+        return ( this.props.abc.map((hoho) => (
+            <div>
             <h3>
                 id: {hoho.id}<br/> 
                 title: {hoho.title} <br/> 
                 completed:{hoho.completed}<br/>
             </h3>
-            </h1>
-        ));
+            <h1>올라 올라 {this.props.da[0].id} </h1>
+            </div>
+            
+        )));
     }
 }
 
