855c9ee49b26c80b79d02aed394eb2bd661cbd42
devneolee <devneolee@gmail.com>
2019-06-04 17:35:04 -0700

    update confusion project

--- react/coursera_react/confusion/src/App.js
+++ react/coursera_react/confusion/src/App.js
@@ -1,17 +1,25 @@
 import React from 'react';
-import { Navbar, NavbarBrand} from 'reactstrap';
-import Menu from './components/MenuComponents';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-      <div>
-      <Navbar dark color="primary">
-        <div className="container">
-          <NavbarBrand href="/">Restorate Con FusionS</NavbarBrand>
-        </div>
-      </Navbar>
-      <Menu />
-      </div>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
