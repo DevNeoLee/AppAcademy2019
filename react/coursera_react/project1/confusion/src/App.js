a80ec0856f3883df447803f24ce0147f9b933eff
DevNeoLee <devneolee@gmail.com>
2019-06-14 13:13:29 -0700

    Initial react_project1

---
+++ react/coursera_react/project1/confusion/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Navbar, NavbarBrand } from 'reactstrap';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+      <Navbar dark color="secondary">
+        
+          <NavbarBrand href="/'">Restaurant Number Three</NavbarBrand>
+        
+      </Navbar>
+      </div>
+    </div>
+  );
+}
+
+export default App;
